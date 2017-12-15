b0f3d028cbb0ca543ba7b32843a90242c519f2ec
desusaiteja <desu.saiteja@gmail.com>
2017-12-14 23:14:31 -0800

    updated port

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ var express = require('express');
 var Client = require('node-rest-client').Client;
 
 var app = express();
+
+
+app.set('port', (process.env.PORT || 9999));
+
 app.use(express.bodyParser());
 app.use("/images", express.static(__dirname + '/images'));
 handlebars  = require('express3-handlebars');
@@ -206,9 +210,15 @@ app.get('/', handle_get ) ;
 app.post('/', handle_post ) ;
 
 
-console.log( "Server running on Port 8080..." ) ;
+//console.log( "Server running on Port 8080..." ) ;
 
-app.listen(8080);
+//app.listen(8080);
+
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
 
 
 /**
